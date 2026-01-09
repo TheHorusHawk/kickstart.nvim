cafcb1a28c91794b9b8490ad11866e21e83e728f
TheHorusHawk <bruno.madureirateles@dedalus.com>
2026-01-09 13:11:26 +0000

    feat: added es_lint as linter

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,6 +6,8 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
+        typescript = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
         markdown = { 'markdownlint' },
       }
 
