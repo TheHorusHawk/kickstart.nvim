92d1cbe6a2f06d69f7dd3f6f4c4f4a8b2c0614c3
TheHorusHawk <bruno.madureirateles@dedalus.com>
2026-07-02 15:03:11 +0100

    feat: relative lines toggler

--- init.lua
+++ init.lua
@@ -195,7 +195,7 @@ vim.o.confirm = true
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+vim.keymap.set({ 'n', 'i', 'v' }, '<leader>r', '<cmd>set relativenumber!<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
