f6a9716e75b15134847ce77052e4f4c43aad98e4
TheHorusHawk <bruno.madureirateles@dedalus.com>
2026-07-02 15:00:54 +0100

    feat: emojis on gitsigns

--- init.lua
+++ init.lua
@@ -300,9 +300,9 @@ require('lazy').setup({
     'lewis6991/gitsigns.nvim',
     opts = {
       signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
+        add = { text = '🚀' },
+        change = { text = '⚙' },
+        delete = { text = '❌' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
