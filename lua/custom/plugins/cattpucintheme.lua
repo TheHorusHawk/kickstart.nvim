7495e5f4c2a28b59ca124dfe6dd6d69ef626a420
TheHorusHawk <bruno.madureirateles@dedalus.com>
2026-08-05 09:38:39 +0100

    feat: changed colorscheme

--- lua/custom/plugins/cattpucintheme.lua
+++ lua/custom/plugins/cattpucintheme.lua
@@ -78,7 +78,7 @@ return {
       },
     }
     -- vim.cmd.colorscheme 'catppuccin-nvim'
-    vim.cmd.colorscheme 'tokyonight-storm'
+    vim.cmd.colorscheme 'tokyonight-moon'
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
   end,
